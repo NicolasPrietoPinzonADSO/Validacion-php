a3b78ba6ec700580b17842277c8b606f17f40312
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2023-06-20 16:57:55 -0500

    feat: :sparkles: Nuevas funciones
    Se añadieron funciones en el php y js

--- recibir.php
+++ recibir.php
@@ -1,31 +1,57 @@
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $telefono = $_POST['telefono'];
-    $correo = $_POST['email'];
-    $edad = $_POST['edad'];
-}
-  if(empty($nombre)){
-    $nombre = "Campo vacio";
-}
-if(empty($apellido)){
-    $apellido = "Campo vacio";
-}
-if(empty($telefono)){
-    $telefono = "Campo vacio";
-}
-if(empty($correo)){
-    $correo = "Campo vacio";
-}
-if(empty($edad)){
-    $edad = "Campo vacio";
-}
-    
-echo "El nombre que ha ingresado es: $nombre <br>";
-echo "El apellido que ha ingresado es: $apellido <br>";
-echo "El telefono que ha ingresado es: $telefono <br>";
-echo "El correo que ha ingresado es: $correo <br>";
-echo "La edad que ha ingresado es: $edad <br>";
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+  <title>Resultados del Formulario</title>
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="text-center mt-5">Resultados del Formulario</h1>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          $nombre = $_POST['nombre'];
+          $apellido = $_POST['apellido'];
+          $telefono = $_POST['telefono'];
+          $correo = $_POST['email'];
+          $edad = $_POST['edad'];
+          $sexo = $_POST['sexo'];
+        }
+
+        if (empty($nombre)) {
+          $nombre = "Campo vacío";
+        }
+        if (empty($apellido)) {
+          $apellido = "Campo vacío";
+        }
+        if (empty($telefono)) {
+          $telefono = "Campo vacío";
+        }
+        if (empty($correo)) {
+          $correo = "Campo vacío";
+        }
+        if (empty($edad)) {
+          $edad = "Campo vacío";
+        }
+        ?>
+
+        <h2 class="mb-3">Datos recibidos:</h2>
+        <p><strong>Nombre:</strong> <?php echo $nombre; ?></p>
+        <p><strong>Apellido:</strong> <?php echo $apellido; ?></p>
+        <p><strong>Teléfono:</strong> <?php echo $telefono; ?></p>
+        <p><strong>Correo:</strong> <?php echo $correo; ?></p>
+        <p><strong>Edad:</strong> <?php echo $edad; ?></p>
+        <p><strong>Sexo:</strong> <?php echo $sexo; ?></p>
+      </div>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
